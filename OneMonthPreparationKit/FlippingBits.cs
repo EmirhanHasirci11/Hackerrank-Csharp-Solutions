437fb95ced579c895f2e71b052b08c88c9aa0dc4
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-13 09:41:46 +0300

    FlippingBits

---
+++ OneMonthPreparationKit/FlippingBits.cs
@@ -0,0 +1,39 @@
+﻿namespace FlippingBits
+{
+    internal class FlippingBits
+    {
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+                long result = FlippingBits.flippingBits(n);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static long flippingBits(long n)
+        {
+            string temp = Convert.ToString(n, 2).PadLeft(32,'0');                       
+            string str = string.Empty;
+            temp.ToList().ForEach(x =>
+            {
+                if (x == '0')
+                {
+                    str += 1;
+                }
+                else
+                {
+                    str += 0;
+                }
+            });
+            return Convert.ToInt64(str, 2);
+
+        }
+
+    }
+}
