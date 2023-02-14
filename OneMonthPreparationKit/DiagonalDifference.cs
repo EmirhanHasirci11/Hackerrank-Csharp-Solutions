c309a544bc017a21135d0016319275399675d5a5
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-15 02:15:33 +0300

    DiagonalDifference

---
+++ OneMonthPreparationKit/DiagonalDifference.cs
@@ -0,0 +1,37 @@
+﻿namespace DiagonalDifference
+{
+    internal class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = DiagonalDifference.diagonalDifference(arr);
+            Console.WriteLine(result);
+        }
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int leftToRight = 0;
+            int indexer = 0;
+            int reverseIndexer = arr[0].Count-1;
+            int rightToLeft= 0;
+            arr.ForEach(arrTemp =>
+            {
+                leftToRight += arrTemp[indexer];
+                indexer++;
+                rightToLeft+= arrTemp[reverseIndexer];
+                reverseIndexer--;
+            });
+            return Math.Abs(leftToRight - rightToLeft);
+        }
+    }
+}
