58743e503e71332997ac7b30ee6f0b0232d3c175
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-15 18:46:39 +0300

    AVeryBigSum

---
+++ ProblemSolutions/AVeryBigSum.cs
@@ -0,0 +1,19 @@
+﻿namespace AVeryBigSum
+{
+    internal class AVeryBigSum
+    {
+        static void Main(string[] args)
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+            long result = aVeryBigSum(ar);
+            Console.WriteLine(result);
+        }
+        public static long aVeryBigSum(List<long> ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
