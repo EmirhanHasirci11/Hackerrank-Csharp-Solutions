773ff45d41fe43cd518b105b9c4a4117051d5543
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-15 19:46:47 +0300

    GradingStudents

---
+++ ProblemSolutions/GradingStudents.cs
@@ -0,0 +1,26 @@
+﻿namespace GradingStudents
+{
+    internal class GradingStudents
+    {
+        static void Main(string[] args)
+        {
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+            List<int> grades = new List<int>();
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+            gradingStudents(grades).ForEach(x => Console.WriteLine(x));
+        }
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            List<int> students = new List<int>();
+            grades.ForEach(grade =>
+            {
+                students.Add(grade >= 38 && grade % 5 >= 3 ? grade = grade + 5 - grade % 5 : grade);              
+            });
+            return students;
+        }
+    }
+}
