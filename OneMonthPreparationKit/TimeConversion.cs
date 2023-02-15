311d3c42038c9aa8f9952b1cc5e86caa9fbe77f8
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-15 15:51:10 +0300

    TimeConversion

---
+++ OneMonthPreparationKit/TimeConversion.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class TimeConversion
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Parse(Console.ReadLine()).ToString("HH:mm:ss"));
+    }
+}
