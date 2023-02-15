bc9bfc519655b3021816ab62caee6b8ed777d1e1
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-15 19:12:03 +0300

    BirthdayCakeCandles

---
+++ ProblemSolutions/BirthdayCakeCandles.cs
@@ -0,0 +1,26 @@
+﻿namespace BirthdayCakeCandles
+{
+    internal class BirthdayCakeCandles
+    {
+        static void Main(string[] args)
+        {
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = birthdayCakeCandles(candles);
+            Console.WriteLine(result);
+        }
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int count = 0;
+            int control = 0;
+            candles.OrderByDescending(x => x).ToList().ForEach(x =>
+            {
+                control = control == 0 ? x : control;
+                count += control == x ? 1 : 0;                                
+            });
+            return count;
+        }
+    }
+}
