3e6216e3f8e50b0f1bd0143a757a35c0911df57a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-15 15:46:10 +0300

    PlusMinus

---
+++ ProblemSolutions/PlusMinus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversionSolution
+{
+    public class PlusMinus
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            PlusMinus.plusMinus(arr);
+        }
+        public static void plusMinus(List<int> arr)
+        {
+            decimal negativeCount = 0;
+            decimal positiveCount = 0;
+            decimal zeroCount = 0;
+            foreach (var indexer in arr)
+            {
+
+                if (indexer == 0)
+                {
+                    zeroCount++;
+                }
+                else if (indexer >= 1)
+                {
+                    positiveCount++;
+                }
+                else
+                {
+                    negativeCount++;
+                }
+            }
+            Console.WriteLine(positiveCount / arr.Count);
+            Console.WriteLine(negativeCount / arr.Count);
+            Console.WriteLine(zeroCount / arr.Count);
+
+        }
+
+
+    }
+}
