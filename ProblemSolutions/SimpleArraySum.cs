8678cc7c5206eead6398e8c9d7a37e9c1c14ad7c
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-15 15:20:41 +0300

    SimpleArraySum

---
+++ ProblemSolutions/SimpleArraySum.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleArraySum
+{
+    internal class SimpleArraySum
+    {
+        static void Main(string[] args)
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+            int result = simpleArraySum(ar);      
+            Console.WriteLine(result);
+        }
+        public static int simpleArraySum(List<int> ar)
+        {
+            return ar.Sum();
+        }
+
+    }
+}
