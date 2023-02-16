72034b315d61e85aa1d54c50761ab9e484ed8ef5
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-16 04:33:27 +0300

    MigratoryBirds

---
+++ ProblemSolutions/MigratoryBirds.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+
+namespace MigratoryBirds
+{
+    internal class MigratoryBirds
+    {
+        static void Main(string[] args)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = migratoryBirds(arr);
+
+            Console.WriteLine(result);
+        }
+        public static int migratoryBirds(List<int> arr)
+        {
+            int[] counts = new int[6];
+            arr.ForEach(x =>
+            {
+                counts[x]++;
+            });
+            int typeCount = counts[1];
+            int mostType = 1;
+            for (int i = 2; i < 6; i++)
+            {
+                if (counts[i]>typeCount)
+                {
+                    typeCount = counts[i];
+                    mostType = i;
+                }
+            }
+            return mostType;            
+        }
+
+    }
+}
