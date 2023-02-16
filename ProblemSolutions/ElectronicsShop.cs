18a6b49e438aeed70cd8f5a2109a3296c2b1784f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-16 20:36:20 +0300

    ElectronicsShop

---
+++ ProblemSolutions/ElectronicsShop.cs
@@ -0,0 +1,48 @@
+﻿namespace ElectronicsShop
+{
+    internal class ElectronicsShop
+    {
+        static void Main(string[] args)
+        {
+            string[] bnm = Console.ReadLine().Split(' ');
+
+            int b = Convert.ToInt32(bnm[0]);
+
+            int n = Convert.ToInt32(bnm[1]);
+
+            int m = Convert.ToInt32(bnm[2]);
+
+            int[] keyboards = Array.ConvertAll(Console.ReadLine().Split(' '), keyboardsTemp => Convert.ToInt32(keyboardsTemp))
+            ;
+
+            int[] drives = Array.ConvertAll(Console.ReadLine().Split(' '), drivesTemp => Convert.ToInt32(drivesTemp))
+            ;
+
+            int moneySpent = getMoneySpent(keyboards, drives, b);
+
+            Console.WriteLine(moneySpent);
+        }
+        static int getMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+            int max = 0;
+            Array.Sort(keyboards);
+            Array.Sort(drives);
+            if (keyboards[0] + drives[0] > b)
+            {
+                return -1;
+            }
+            else
+            {
+                foreach(var keyboard in keyboards)
+                {
+                    foreach (var drive in drives)
+                    {
+                        max =drive+keyboard<=b ?Math.Max(max, keyboard + drive):max;
+                    }
+                }
+            }
+            return max;
+
+        }
+    }
+}
