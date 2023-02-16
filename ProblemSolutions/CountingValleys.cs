164a5fa0bbb94d22e13ea58f39a3c457aa972792
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-16 19:49:35 +0300

    CountingValleys

---
+++ ProblemSolutions/CountingValleys.cs
@@ -0,0 +1,29 @@
+﻿namespace CountingValleys
+{
+    internal class CountingValleys
+    {
+        static void Main(string[] args)
+        {
+            int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string path = Console.ReadLine();
+
+            int result = countingValleys(steps, path);
+            Console.WriteLine(result);
+        }
+        public static int countingValleys(int steps, string path)
+        {
+            int level = 0;
+            int previousLevel;
+            int valleyCount = 0;
+            for (int i = 0; i < steps; i++)
+            {
+                previousLevel = level;
+                level += path[i] == 'U' ? 1 : -1;
+                valleyCount += level == 0 && previousLevel == -1 ? 1 : 0;
+
+            }
+            return valleyCount;
+        }
+    }
+}
