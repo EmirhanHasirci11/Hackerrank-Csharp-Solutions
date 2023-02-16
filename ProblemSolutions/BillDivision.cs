6767c4d4f9f321111958a57b5a8f6ce29a4357f1
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-16 16:45:44 +0300

    BillDivision

---
+++ ProblemSolutions/BillDivision.cs
@@ -0,0 +1,27 @@
+﻿
+namespace BillDivision
+{
+    internal class BillDivision
+    {
+        static void Main(string[] args)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+            int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+            bonAppetit(bill, k, b);
+        }
+        public static void bonAppetit(List<int> bill, int k, int b)
+        {
+            int debt= ((bill.Sum() - bill[k]) / 2) -b;
+            Console.WriteLine(debt==0?"Bon Appetit":Math.Abs(debt));
+            
+        }
+    }
+}
