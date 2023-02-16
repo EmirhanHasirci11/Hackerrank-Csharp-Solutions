95798c61000fa005e1034b0a379d187a6d67adf6
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-16 19:12:15 +0300

    DrawingBook

---
+++ ProblemSolutions/DrawingBook.cs
@@ -0,0 +1,21 @@
+﻿namespace DrawingBook
+{
+    internal class DrawingBook
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            int p = Convert.ToInt32(Console.ReadLine().Trim());
+            int result = pageCount(n, p);
+            Console.WriteLine(result);
+
+
+        }
+
+        public static int pageCount(int n, int p)
+        {
+            return Math.Min(p/2, n/2-p/2);            
+        }
+    }
+}
