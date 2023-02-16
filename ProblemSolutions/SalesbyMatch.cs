e160ed4d24431735c98d4f90857a7ba05be09f3f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-16 17:17:56 +0300

    SalesbyMatch

---
+++ ProblemSolutions/SalesbyMatch.cs
@@ -0,0 +1,48 @@
+﻿namespace SalesbyMatch
+{
+    internal class SalesbyMatch
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = sockMerchant(n, ar);
+            Console.WriteLine(result);
+        }
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int pairs = 0;
+            Dictionary<int,int> keyValuePairs= new Dictionary<int,int>();
+            ar.ForEach(arTemp =>
+            {
+                if (!keyValuePairs.ContainsKey(arTemp))
+                {
+                    keyValuePairs.Add(arTemp, 1);
+                }
+                else
+                {
+                    keyValuePairs[arTemp]++;
+                }
+            });
+            foreach(var item in keyValuePairs.Values)
+            {
+                if (item>1)
+                {
+                    int temp = item;
+                    if (item % 2 != 0)
+                    {
+                        temp--;
+                    }
+                    while (temp != 0) {
+                        pairs++;
+                        temp -= 2;
+                    }
+                   
+                }
+            }
+            return pairs;
+        }
+    }
+}
