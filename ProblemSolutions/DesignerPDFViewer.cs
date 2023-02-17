6ba53b805593a3d752cae26fe8dc88c1aace5bd7
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-17 06:11:30 +0300

    DesignerPDFViewer

---
+++ ProblemSolutions/DesignerPDFViewer.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignerPDFViewer
+{
+    internal class DesignerPDFViewer
+    {
+        static void Main(string[] args)
+        {
+            List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+            string word = Console.ReadLine();
+
+            int result = designerPdfViewer(h, word);
+            Console.WriteLine(result);
+        }
+        public static int designerPdfViewer(List<int> h, string word)
+        {
+            int maxHeight = 0;
+            foreach(char c in word)
+            {
+                maxHeight = h[c-'a'] > maxHeight ? h[c - 'a'] : maxHeight;
+            }
+            return maxHeight*word.Length;
+        }
+    }
+}
