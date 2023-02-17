f8c847444cb21b7466047db79a1a6243eed78840
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-17 06:26:23 +0300

    UtopianTree

---
+++ ProblemSolutions/UtopianTree.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace UtopianTree
+{
+    internal class UtopianTree
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                int result = utopianTree(n);
+
+                Console.WriteLine(result);
+            }
+        }
+        public static int utopianTree(int n)
+        {
+            int height = 1;
+            for (int i = 1; i <=n; i++)
+            {
+                height = i % 2 == 0 ? height+1 : height * 2;
+            }
+            return height;
+        }
+    }
+}
