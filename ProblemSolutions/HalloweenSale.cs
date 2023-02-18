bc38fc452d42c8bac6b7ebe1cc20f8a6067ac2df
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-18 04:17:45 +0300

    HalloweenSale

---
+++ ProblemSolutions/HalloweenSale.cs
@@ -0,0 +1,43 @@
+﻿namespace HalloweenSale
+{
+    internal class HalloweenSale
+    {
+        static void Main(string[] args)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int p = Convert.ToInt32(firstMultipleInput[0]);
+
+            int d = Convert.ToInt32(firstMultipleInput[1]);
+
+            int m = Convert.ToInt32(firstMultipleInput[2]);
+
+            int s = Convert.ToInt32(firstMultipleInput[3]);
+
+            int answer = howManyGames(p, d, m, s);
+
+            Console.WriteLine(answer);
+        }
+        public static int howManyGames(int p, int d, int m, int s)
+        {
+            int control = p;
+            int gameCount = 0;
+            while (s > 0)
+            {
+                if (s - control >= 0)
+                {
+                    s -= control;
+                    control = control - d > m ? control - d : m;                    
+                    gameCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return gameCount;
+
+        }
+
+    }
+}
