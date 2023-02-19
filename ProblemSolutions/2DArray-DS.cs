092856609e43d695273527d8f1225920b4899caf
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-19 19:28:58 +0300

    2DArray-DS

---
+++ ProblemSolutions/2DArray-DS.cs
@@ -0,0 +1,32 @@
+﻿namespace _2DArray_DS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> arr = new List<List<int>>();
+            for (int i = 0; i < 6; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+            int result = hourglassSum(arr);
+            Console.WriteLine(result);
+        }
+        public static int hourglassSum(List<List<int>> arr)
+        {
+            int sum = Int32.MinValue;
+            for(int i=0;i<=3;i++)
+            {
+                int temp = 0;
+                for(int j = 2; j < 6; j++)
+                {
+                    temp = arr[i][j] + arr[i][j - 1] + arr[i][j - 2];
+                    temp += arr[i + 1][j - 1];
+                    temp += arr[i + 2][j] + arr[i + 2][j - 1] + arr[i + 2][j - 2];
+                    sum = Math.Max(sum, temp);
+                }
+            }
+            return sum;
+        }
+    }
+}
