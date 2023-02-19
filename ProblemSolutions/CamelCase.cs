da3c401f98083fdc4e975434d55d8318c000a590
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-19 16:48:45 +0300

    CamelCase

---
+++ ProblemSolutions/CamelCase.cs
@@ -0,0 +1,23 @@
+﻿namespace CamelCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int result = camelcase(s);
+            
+            Console.WriteLine(result);
+        }
+        public static int camelcase(string s)
+        {
+            int count = 1;
+            foreach (char c in s)
+            {
+                count += Char.IsUpper(c) == true ? 1 : 0;
+            }
+            return count;
+        }
+    }
+}
