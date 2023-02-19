f7bcda0ff004f402fcf25dffdc46cb67c1dd404d
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-19 16:27:35 +0300

    IntroToTutorialChallenges

---
+++ ProblemSolutions/IntroToTutorialChallenges.cs
@@ -0,0 +1,22 @@
+﻿namespace IntroToTutorialChallenges
+{
+    internal class IntroToTutorialChallenges
+    {
+        static void Main(string[] args)
+        {
+            int V = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = introTutorial(V, arr);
+            Console.WriteLine(result);
+
+        }
+         public static int introTutorial(int V, List<int> arr)
+        {
+            return arr.IndexOf(V);
+        }
+    }
+}
