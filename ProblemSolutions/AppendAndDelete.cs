e7bd49fb299e36e2da548f48f09b165d6f7b68a7
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-19 04:52:43 +0300

    AppendAndDelete

---
+++ ProblemSolutions/AppendAndDelete.cs
@@ -0,0 +1,31 @@
+﻿namespace AppendAndDelete
+{
+    internal class AppendAndDelete
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string t = Console.ReadLine();
+
+            int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string result = appendAndDelete(s, t, k);
+
+            Console.WriteLine(result);
+        }
+        public static string appendAndDelete(string s, string t, int k)
+        {
+            int n = s.Length, m = t.Length, i = 0;
+
+            if (k >= m + n) {  return "Yes"; }
+
+            while (i < Math.Min(m, n) && s[i] == t[i]) i++;                        
+
+            k -= m - i + n - i;
+            return k >= 0 && k % 2 == 0 ? "Yes" : "No";
+
+
+        }
+    }
+}
