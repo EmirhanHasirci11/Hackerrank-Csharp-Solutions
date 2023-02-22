2d9f5b5c07715519dbe8d76e7d07695d73ed36ea
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-22 10:15:29 +0300

    CorrectnessAndTheLoopInvariant

---
+++ ProblemSolutions/CorrectnessAndTheLoopInvariant.cs
@@ -0,0 +1,28 @@
+﻿namespace CorrectnessAndTheLoopInvariant
+{
+    internal class CorrectnessAndTheLoopInvariant
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int[] _ar = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+            insertionSort(_ar);
+        }
+        public static void insertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int indexer = i - 1;
+                while (indexer >= 0 && arr[indexer] > key)
+                {
+                    arr[indexer + 1] = arr[indexer];
+                    indexer--;
+
+                }
+                arr[indexer + 1] = key;
+            }
+            arr.ToList().ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
