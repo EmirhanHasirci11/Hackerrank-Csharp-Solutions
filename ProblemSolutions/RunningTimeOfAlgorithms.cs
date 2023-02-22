8fe12f4518bc19c9f105b93d5ebf64cbe7797a76
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-22 10:19:36 +0300

    RunningTimeOfAlgorithms

---
+++ ProblemSolutions/RunningTimeOfAlgorithms.cs
@@ -0,0 +1,33 @@
+﻿namespace RunningTimeOfAlgorithms
+{
+    internal class RunningTimeOfAlgorithms
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = runningTime(arr);
+            Console.WriteLine(result);
+        }
+        public static int runningTime(List<int> arr)
+        {
+            int shiftCount = 0;
+            for (int i = 1; i < arr.Count; i++)
+            {
+                int key = arr[i];
+                int indexer = i - 1;
+                while (indexer >= 0 && arr[indexer] > key)
+                {
+                    arr[indexer + 1] = arr[indexer];
+                    indexer--;
+                    shiftCount++;
+                }
+                arr[indexer + 1] = key;
+            }
+           return shiftCount;
+        }
+
+    }
+}
