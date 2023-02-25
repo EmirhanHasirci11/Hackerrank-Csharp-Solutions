b94d18be2787c8a96b5080311dfa8698428376ec
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-25 07:34:35 +0300

    RepeatedString

---
+++ ProblemSolutions/RepeatedString.cs
@@ -0,0 +1,21 @@
+﻿namespace RepeatedString
+{
+    internal class RepeatedString
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+            long result = repeatedString(s, n);
+            Console.WriteLine(result);
+        }
+        public static long repeatedString(string s, long n)
+        {
+            int count = s.Where(x => x == 'a').Count();           
+            long remaining = n % s.Length;
+            string str = s.Substring(0,(int)remaining);
+            return n%s.Length==0? count * (n / s.Length) : str.Where(x=>x=='a').Count()+((n-remaining)/s.Length)*count;
+        }
+
+    }
+}
