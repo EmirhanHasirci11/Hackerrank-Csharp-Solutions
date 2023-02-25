56c28c2507be7493b282f335f528e3d84a5bc13a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-02-25 20:54:23 +0300

    ExtraLongFactorials

---
+++ ProblemSolutions/ExtraLongFactorials.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    internal class ExtraLongFactorials
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine(extraLongFactorials(n));
+        }
+        public static BigInteger extraLongFactorials(int n)
+        {            
+            return n == 1 ? 1 : n * extraLongFactorials(n - 1);
+        }
+    }
+}
