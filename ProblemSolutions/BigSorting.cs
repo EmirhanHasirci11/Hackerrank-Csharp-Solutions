a23fb1794fa3cdc79ebfe12c8fa829163dfd3520
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-03-26 01:17:53 +0300

    BigSorting

---
+++ ProblemSolutions/BigSorting.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Numerics;
+
+namespace BigSorting
+{
+    internal class BigSorting
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> unsorted = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string unsortedItem = Console.ReadLine();
+                unsorted.Add(unsortedItem);
+            }
+            bigSorting(unsorted).ForEach(x=>Console.WriteLine(x));
+
+           
+
+        }
+        public static List<string> bigSorting(List<string> unsorted)
+        {
+            unsorted = unsorted.OrderBy(x => x).ToList();
+            unsorted = unsorted.OrderBy(x => x.Length).ToList();
+            return unsorted;
+        }
+    }
+}
